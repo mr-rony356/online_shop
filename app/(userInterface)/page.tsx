51c0645e3119b53689e2db8426dde06acd13a1bc
Omor Faruk Rony <omorfarukrony2@gmail.com>
2024-05-17 03:46:50 +0600

    Skeleton loading add

--- app/(userInterface)/page.tsx
+++ app/(userInterface)/page.tsx
@@ -1,34 +1,47 @@
 import SpecialProductCarousel from "./components/SpecialProducts";
 import PageHeader from "../../components/ui/PageHeader";
 import CategoryCard from "./components/CategoryCard";
-import db from "@/db/db";
-import Link from "next/link";
 import ProductsGrid from './products/_components/ProductsGrid';
+import { getPopularProducts, getNewetProducts } from "@/lib/product";
+import { Suspense } from "react";
 
-function getPopularProducts() {
-  return db.product.findMany({
-    where: { isAvailableForPurchase: true },
-    orderBy: { orders: { _count: "desc" } },
-    take: 6,
-  });
-}
+export default async function Home() {
+  const popularProducts = await getPopularProducts();
+  const newestProducts = await getNewetProducts();
 
-function getNewetProducts() {
-  return db.product.findMany({
-    where: { isAvailableForPurchase: true },
-    orderBy: { createdAt: "desc" },
-    take: 6,
-  });
-}
-
-const SkeletonProductsGrid = () => {
+  return (
+    <main className="flex flex-col gap-16 ">
+      <SpecialProductCarousel />
+      <div>
+        <PageHeader title="Categories" />
+        <div className="grid grid-cols-2 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-6 gap-8 mt-8">
+          <CategoryCard name="T-shirt" image="/men_clothes.svg" />
+          <CategoryCard name="Mobiles" image="/mobile.svg" />
+          <CategoryCard name="Laptops" image="/laptop.svg" />
+          <CategoryCard name="Headphones" image="/headphones.svg" />
+          <CategoryCard name="Watchs" image="/watche.svg" />
+          <CategoryCard name="Coming Soon" image="/men_clothes.svg" />
+        </div>
+      </div>
+      <Suspense fallback={<SkeletonProductsGrid />}>
+        <ProductsGrid products={popularProducts} title="Most Popular" />
+      </Suspense>
+      <Suspense fallback={<SkeletonProductsGrid />}>
+        <ProductsGrid products={newestProducts} title="Newest" />
+      </Suspense>
+    </main>
+  );
+}const SkeletonProductsGrid = () => {
   return (
     <div>
       <div className="flex gap-4 items-center ">
         <h1 className="text-2xl font-bold"></h1>
-        <Link href="/products" className="flex items-center cursor-pointer gap-1 text-sm">
-          {' '}
-        </Link>
+        <a
+          href="/products"
+          className="flex items-center cursor-pointer gap-1 text-sm"
+        >
+          {" "}
+        </a>
       </div>
       <div className="grid grid-cols-2 sm:grid-cols-3 md:grid-cols-4 lg:grid-cols-5 my-8 gap-8 ">
         {Array.from({ length: 5 }).map((_, index) => (
@@ -45,25 +58,3 @@ const SkeletonProductsGrid = () => {
     </div>
   );
 };
-
-
-export default function Home() {
-  return (
-    <main className="flex flex-col gap-16 ">
-      <SpecialProductCarousel />
-      <div>
-        <PageHeader title="Categories" />
-        <div className="grid grid-cols-2 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-6 gap-8 mt-8">
-          <CategoryCard name="T-shirt" image="/men_clothes.svg" />
-          <CategoryCard name="Mobiles" image="/mobile.svg" />
-          <CategoryCard name="Laptops" image="/laptop.svg" />
-          <CategoryCard name="Headphones" image="/headphones.svg" />
-          <CategoryCard name="Watchs" image="/watche.svg" />
-          <CategoryCard name="Coming Soon" image="/men_clothes.svg" />
-        </div>
-      </div>
-      <ProductsGrid fetchProduct={getPopularProducts} title="Most Popular" />
-      <ProductsGrid fetchProduct={getNewetProducts} title="Newest" />
-    </main>
-  );
-}
--- app/(userInterface)/products/_components/ProductsGrid.tsx
+++ app/(userInterface)/products/_components/ProductsGrid.tsx
@@ -1,3 +1,4 @@
+// app/products/_components/ProductsGrid.tsx
 import PageHeader from "@/components/ui/PageHeader";
 import { formatCurrency } from "@/lib/formatters";
 import { Product } from "@prisma/client";
@@ -6,42 +7,27 @@ import Image from "next/image";
 import Link from "next/link";
 
 interface ProductgridProps {
-  fetchProduct: () => Promise<Product[]>;
+  products: Product[];
   title: string;
 }
 
-const ProductsGrid = async ({ fetchProduct, title }: ProductgridProps) => {
-  const products = await fetchProduct();
-
+const ProductsGrid = ({ products, title }: ProductgridProps) => {
   return (
     <div>
       <div className="flex gap-4 items-center ">
         <PageHeader title={title} />
-        <Link
-          href="/products"
-          className="flex items-center cursor-pointer gap-1 text-sm"
-        >
+        <Link href="/products" className="flex items-center cursor-pointer gap-1 text-sm">
           View All <ArrowRight className="size-4"></ArrowRight>
         </Link>
       </div>
       <div className="grid grid-cols-2 sm:grid-cols-3 md:grid-cols-4 lg:grid-cols-5 my-8 gap-8 ">
         {products.map((product) => (
-          <div
-            key={product.id}
-            className="flex flex-col justify-evenly gap-2  border-2 border-gray-100 rounded-lg p-4 transform transition-transform hover:scale-105 cursor-pointer"
-          >
-            <div className="bg-gray-100 p-4 min-h-40 flex flex-col items-center justify-center max-h-40  rounded-lg">
-              <Image
-                src={product.imagePath}
-                width={150}
-                height={100}
-                alt={product.name}
-              />
+          <div key={product.id} className="flex flex-col justify-evenly gap-2 border-2 border-gray-100 rounded-lg p-4 transform transition-transform hover:scale-105 cursor-pointer">
+            <div className="bg-gray-100 p-4 min-h-40 flex flex-col items-center justify-center max-h-40 rounded-lg">
+              <Image src={product.imagePath} width={150} height={100} alt={product.name} />
             </div>
             <h1>{product.name}</h1>
-            <p className="font-bold text-orange-600">
-              {formatCurrency(product.priceInCents / 100)}
-            </p>
+            <p className="font-bold text-orange-600"> {formatCurrency(product.priceInCents / 100)} </p>
           </div>
         ))}
       </div>
